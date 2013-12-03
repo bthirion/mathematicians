60cb6a11453221b3c610056e5c1ef2b800df657a
bthirion <bertrand.thirion@inria.fr>
2013-12-03 18:22:26 +0100

    Debugged the registration problem

--- viz_activation.py
+++ viz_activation.py
@@ -1,6 +1,8 @@
 """Visualize the mesh using Mayavi
 
 Author: Bertrand Thirion, 2013
+
+export SUBJECTS_DIR=''
 """
 import os
 import nibabel.freesurfer as nf
@@ -10,6 +12,7 @@ from nibabel.gifti import read
 from nibabel import load
 from nipy.labs import viz3d
 from nipy.labs.viz import cm
+import commands
 
 subjects = ['aa130114', 'jl120341', 'mp130263', 'aa130169', 'jl130200',
             'mr120371', 'al130244', 'kg120369', 'nl120167', 'bm120103',
@@ -26,7 +29,7 @@ subject_dir = os.path.join(work_dir, subject)
 t1_dir = os.path.join(subject_dir, 't1')
 surf_dir = os.path.join(t1_dir, subject, 'surf')
 fun_dir = os.path.join(subject_dir, 'fmri/results')
-contrast = 'true-false' #'visual' # 'audio' # 'motor' # 'reflection' #
+contrast = 'visual' # 'true-false' #'audio' # 'motor' # 'reflection' #
 
 THRESHOLD = 3.
 
@@ -45,7 +48,7 @@ for hemisphere in ['l', 'r']:
     name = ''
     cmin = -1
     cmax = 1
-    mlab.triangular_mesh(x, y, z, triangles, transparent=False, opacity=1.,
+    mlab.triangular_mesh(x, y, z, triangles, transparent=True, opacity=1.,
                          name=name, scalars=curv, colormap="bone",
                          vmin=cmin, vmax=cmax)
     func_mesh = mlab.pipeline.triangular_mesh_source(
@@ -53,23 +56,21 @@ for hemisphere in ['l', 'r']:
     thresh = mlab.pipeline.threshold(func_mesh, low=3.)
     mlab.pipeline.surface(thresh, colormap="hot", vmin=THRESHOLD, vmax=8)
 
-"""
-talairach_tf = os.path.join(t1_dir, subject, 'mri/transforms/talairach.xfm')
-talairach_affine =[
-    [1.103738, -0.011035, 0.015324, 0.331757],
-    [0.007372, 1.031341, 0.020136, -32.550781],
-    [-0.028529, 0.008960, 1.121604, -18.154022],
-    [0, 0, 0, 1]]
-# talairach_affine = np.loadtxt(talairach_tf)
 
+# Plot the MRI image on top of the meshes
+ref = os.path.join(t1_dir, subject, 'mri/orig.mgz')
+norig = commands.getoutput('mri_info --vox2ras %s' %ref)
+norig = np.array([x.split() for x in norig.split('\n')]).astype(np.float)
+torig = commands.getoutput('mri_info --vox2ras-tkr %s' %ref)
+torig = np.array([x.split() for x in torig.split('\n')]).astype(np.float)
+aff = np.linalg.inv(np.dot(norig, np.linalg.inv(torig)))
 
 vol_file = os.path.join(fun_dir, '%s_z_map.nii' % contrast)
 data_ = load(vol_file).get_data()
-affine = load(vol_file).get_affine()
-affine_comp = np.dot(talairach_affine, affine)
-
-viz3d.plot_map_3d(data_, affine_comp, cmap=cm.cold_hot,
+fun_affine = load(vol_file).get_affine()
+aff_ = np.dot(aff, fun_affine)
+viz3d.plot_map_3d(data_, aff_, cmap=cm.cold_hot,
                   vmin=-8, vmax=8, anat=False, threshold=THRESHOLD)
-"""
-mlab.show()
+
+# mlab.show()
 
